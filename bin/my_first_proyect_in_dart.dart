364aa3a421fae2c4f1e43d6491ad0d403d3e7f39
YtzRawr <matiaspizarro133@gmail.com>
2022-12-21 21:04:49 -0300

    start in dart

---
+++ bin/my_first_proyect_in_dart.dart
@@ -0,0 +1,5 @@
+import 'package:my_first_proyect_in_dart/my_first_proyect_in_dart.dart' as my_first_proyect_in_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${my_first_proyect_in_dart.calculate()}!');
+}
