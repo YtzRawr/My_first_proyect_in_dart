6ed70c0587d467985b467e72594e64971e746698
YtzRawr <matiaspizarro133@gmail.com>
2022-12-22 17:04:41 -0300

    coments

--- bin/my_first_proyect_in_dart.dart
+++ bin/my_first_proyect_in_dart.dart
@@ -2,7 +2,9 @@ import 'package:my_first_proyect_in_dart/my_first_proyect_in_dart.dart'
     as my_first_proyect_in_dart;
 
 void main(List<String> arguments) {
+  // var prueba
   var prueba = 'Chanchito feliz';
+  // var prueba2
   var prueba2 = 20;
   print('Hello world: ${my_first_proyect_in_dart.calculate()}!');
   print('<3 ' + prueba);
