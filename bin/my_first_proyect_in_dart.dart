964eddf2c441c4f235baab3f0d409e4ecc23f524
YtzRawr <matiaspizarro133@gmail.com>
2022-12-21 21:18:37 -0300

    variables

--- bin/my_first_proyect_in_dart.dart
+++ bin/my_first_proyect_in_dart.dart
@@ -1,5 +1,10 @@
-import 'package:my_first_proyect_in_dart/my_first_proyect_in_dart.dart' as my_first_proyect_in_dart;
+import 'package:my_first_proyect_in_dart/my_first_proyect_in_dart.dart'
+    as my_first_proyect_in_dart;
 
 void main(List<String> arguments) {
+  var prueba = 'Chanchito feliz';
+  var prueba2 = 20;
   print('Hello world: ${my_first_proyect_in_dart.calculate()}!');
+  print('<3 ' + prueba);
+  print(prueba2);
 }
