6852204a279b827068b900dce86fb3b29ab9e44f
YtzRawr <matiaspizarro133@gmail.com>
2022-12-22 16:58:20 -0300

    runtimetype

--- bin/my_first_proyect_in_dart.dart
+++ bin/my_first_proyect_in_dart.dart
@@ -6,5 +6,5 @@ void main(List<String> arguments) {
   var prueba2 = 20;
   print('Hello world: ${my_first_proyect_in_dart.calculate()}!');
   print('<3 ' + prueba);
-  print(prueba2);
+  print(prueba2.runtimeType);
 }
